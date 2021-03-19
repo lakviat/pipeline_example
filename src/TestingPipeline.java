22e8f0bc27f5b08edd98ae4e378fa4d7fd0166e2
Nurlan Dzhunushaliev <nurlandzhunushaliev@Nurlans-MBP.lan>
2021-03-19 17:14:52 -0500

    example

---
+++ src/TestingPipeline.java
@@ -0,0 +1,6 @@
+public class TestingPipeline {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("This supposed to build in pipeline");
+    }
+}

7869b24883c3eb722ce893122fcf57f7a733ef77
lakviat@yahoo.com <Nurlan259683>
2021-03-19 17:56:57 -0500

    adding gradle wrapper

--- src/TestingPipeline.java
+++ src/TestingPipeline.java
@@ -1,4 +1,5 @@
 public class TestingPipeline {
+
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("This supposed to build in pipeline");
